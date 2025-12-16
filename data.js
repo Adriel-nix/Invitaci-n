5f24c4e9889aa1e7e5e324a16f927e97838c9a0f
adriel <nix19209@gmail.com>
2025-12-15 21:06:46 -0600

    Propuesta1

---
+++ data.js
@@ -0,0 +1,16 @@
+const invitacionData = {
+  titulo: "CÉSAR & RAQUEL",
+  fecha: "Domingo 12 de abril 2026",
+  hora: "3:30 PM",
+  lugar: "V Entertainment, Heredia",
+  versiculo: "“El amor nunca se da por vencido...” 1 Corintios 13:7",
+  adultos: "Evento exclusivo para adultos. No mascotas.",
+  vestimenta: "Formal con estilo romántico",
+  regalo: "Si deseas hacernos un regalo, agradecemos que sea en efectivo.",
+  sinpe: "+506 8954-6165",
+  ibanCRC: "CR27010200009211878867",
+  ibanUSD: "CR20010200009326431822",
+  confirmarAntes: "01 de febrero 2026",
+  waze: "https://waze.com/ul/hd1u1mbpx7",
+  formulario: "https://docs.google.com/forms/d/e/1FAIpQLSeajjd7BVdWg5zn_P-QgxrUpCVWFzG_402njz_Yc3Uf1yMS2A/viewform"
+};
